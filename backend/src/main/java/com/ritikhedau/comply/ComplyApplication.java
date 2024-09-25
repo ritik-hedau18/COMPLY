2c921cfc79bbf05840270042241dad455a4399f8
ritik-hedau18 <ritikhedau253@gmail.com>
2024-09-25 11:04:00 +0530

    feat: create service interface and business logic implementations

---
+++ backend/src/main/java/com/ritikhedau/comply/ComplyApplication.java
@@ -0,0 +1,11 @@
+package com.ritikhedau.comply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ComplyApplication.class, args);
+    }
+}
